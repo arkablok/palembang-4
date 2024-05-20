2641e2065163692df4a3a5fe6397f166a343d364
Athaayafn <nuhafatin30@gmail.com>
2024-05-20 15:43:54 +0700

    Commit9

--- lineChart.js
+++ lineChart.js
@@ -1,31 +1,38 @@
-const labels = ['Jan','Feb','Maret','April','Mei','Juni','July','Agust','Sept','Okto','Nov','Des'];
+fetch('lineChartData.json')
+  .then(response => response.json())
+  .then(data => {
+  
+    const labels = data.map(item => item.month);
+    const transactions = data.map(item => parseInt(item.totalTransaksi));
 
-const myLineChart = document.getElementById('lineChart').getContext('2d');
+    const myLineChart = document.getElementById('lineChart').getContext('2d');
 
-new Chart(myLineChart, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            axis: 'x',
-            label: 'Produk Terjual',
-            data: [873.5,897.25,1108.5,1615.0,1564.5,1782.25,2105.25,2011.25,1641.0,1595.75,1489.25,1361.75],
-            fill: false,
-            backgroundColor: "rgba(255, 137, 28)",
-            borderColor: "rgba(255, 165, 0)",
-            borderWidth: 1
-        }]
-    },
-    options: {
-      indexAxis: 'x',
-      scales: {
-        y: {
-          beginAtZero: true,
-          title: {
-            display: true,
-            text: 'produk terjual',
+    new Chart(myLineChart, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                axis: 'x',
+                label: 'Produk Terjual',
+                data: transactions,
+                fill: false,
+                backgroundColor: "rgba(255, 137, 28)",
+                borderColor: "rgba(255, 165, 0)",
+                borderWidth: 1
+            }]
+        },
+        options: {
+          indexAxis: 'x',
+          scales: {
+            y: {
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'produk terjual',
+              }
+            }
           }
         }
-      }
-    }
-});
+    });
+  }
+)
